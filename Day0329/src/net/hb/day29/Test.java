e92c4c40f74e6cacafc8f18167c428869b1ecb94
lee <meaning1123@naver.com>
2021-04-02 10:18:03 +0900

    04-02 오전 10:16 정민조원

---
+++ Day0329/src/net/hb/day29/Test.java
@@ -0,0 +1,13 @@
+package net.hb.day29;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int i=0;
+		for(int a=0; a<10; a++) {
+			i=i+1;
+					System.out.println(i+" ");
+		}
+	}
+}
+	
